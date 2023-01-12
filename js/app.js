04ab581045536c57db8c920e11277fb82f579774
taalaibekovdeg <tilektaalaibekovich@gmail.com>
2023-01-12 23:18:11 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// const btn = document.querySelector(".hero--agree__click")
+// btn.addEventListener("click", (e) => {
+//     if (!e.target.classList.parentNode.("")) {
+//        btn.style.background = "red"
+//     }else {
+//
+//     }
+// })
+
